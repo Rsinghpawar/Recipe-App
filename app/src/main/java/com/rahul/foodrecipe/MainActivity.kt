ab773c8b37701ae800453b3d52a0e5edc3fc3ca9
Rahul <rahulkmr.pil@gmail.com>
2020-12-14 22:02:50 +0530

    created item for recyclerview

--- app/src/main/java/com/rahul/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/rahul/foodrecipe/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
         bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController , appBarConfiguration)
 
     }
 
