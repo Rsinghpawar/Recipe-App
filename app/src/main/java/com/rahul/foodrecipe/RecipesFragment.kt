377a5bd3890d68d51cb9b8dda1aa29ab1c152f6a
Rahul <rahulkmr.pil@gmail.com>
2020-12-18 14:22:43 +0530

    created item for recyclerview

--- app/src/main/java/com/rahul/foodrecipe/RecipesFragment.kt
+++ app/src/main/java/com/rahul/foodrecipe/RecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.*
 
 
 class RecipesFragment : Fragment() {
@@ -18,4 +19,8 @@ class RecipesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recipes, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recycler_view.showShimmer()
+    }
 }
